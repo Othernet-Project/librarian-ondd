4476c52a9ed58f66c682700a298023fff7d896b7
Branko Vukelic <branko@outernet.is>
2015-10-21 17:29:21 +0200

    Next can skip tuning, Test is used to erm... test
    With this change, the tuner settings can be tested without going to next step
    by using the "Test settings" button. Submitting with this button causes the
    wizard to remain at ONDD step indefinitely. Using Next no longer requires full
    form validation, and stores empty dict instead of preset data if the data is
    invalid, proceeding to next step without an error.
    
    The wizard is also modified to ack empty dict as valid configuration data.
    
    See #4

--- librarian_ondd/setup.py
+++ librarian_ondd/setup.py
@@ -1,6 +1,7 @@
 import logging
 
 from bottle import request
+from bottle_utils.i18n import lazy_gettext as _
 
 from .forms import ONDDForm
 
@@ -11,11 +12,20 @@ except AttributeError:
 
 
 def read_ondd_setup():
-    initial_data = request.app.supervisor.exts.setup.get('ondd', {})
+    initial_data = request.app.supervisor.exts.setup.get('ondd')
     return {} if isinstance(initial_data, bool) else initial_data
 
 
 def has_invalid_config():
+    settings = read_ondd_setup()
+    if settings is None:
+        # Settings is None if ONDD configuration has never been performed
+        return True
+    if settings == {}:
+        # Settings is a dict if settings has been performed by no preset is
+        # present. This is allowed, as user is allowed to skip through the
+        # setup step without setting the tuner settings.
+        return False
     form = ONDDForm(read_ondd_setup())
     return not form.is_valid()
 
@@ -25,10 +35,28 @@ def setup_ondd_form():
 
 
 def setup_ondd():
+    is_test_mode = request.forms.get('mode', 'submit') == 'test'
+
     form = ONDDForm(request.forms)
-    if not form.is_valid():
+    form_valid = form.is_valid()
+
+    if form_valid:
+        # Store full settings
+        logging.info('ONDD: tuner settings updated')
+        request.app.supervisor.exts.setup.append({'ondd': form.processed_data})
+
+        if is_test_mode:
+            return dict(successful=False, form=form, status=ipc.get_status(),
+                        # Translators, shown when tuner settings are updated
+                        # during setup wizard step.
+                        message=_('Tuner settings have been updated'))
+        return dict(successful=True)
+
+    # Form is not valid
+
+    if is_test_mode:
+        # We only do something about this in test mode
         return dict(successful=False, form=form, status=ipc.get_status())
 
-    logging.info('ONDD: tuner settings updated')
-    request.app.supervisor.exts.setup.append({'ondd': form.processed_data})
+    request.app.supervisor.exts.setup.append({'ondd': {}})
     return dict(successful=True)
