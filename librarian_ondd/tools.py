0824300ed84c37831f580201a332f221c8b0465a
Andrean Franc <andrean.franc@gmail.com>
2015-10-21 14:50:41 +0200

    Make has_tuner a template_helper as well

--- librarian_ondd/tools.py
+++ librarian_ondd/tools.py
@@ -1,6 +1,9 @@
 import os
 
+from librarian_core.contrib.templates.decorators import template_helper
 
+
+@template_helper
 def has_tuner():
     TUNER_DEV_PATH = '/dev/dvb/adapter0/frontend0'
     return os.path.exists(TUNER_DEV_PATH)
