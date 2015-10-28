4e2284b13705057de0b3e33999a57aa68f6a46a0
Ben Ennis <ben@outernet.is>
2015-10-28 08:15:42 -0700

    recompiled static assets

--- librarian_ondd/static/js/ondd_presets.js
+++ librarian_ondd/static/js/ondd_presets.js
@@ -28,11 +28,9 @@
     transponders = onddForm.find('#transponders');
     val = transponders.val();
     opt = options[val];
-    console.log(val);
     help = transponders.next('.o-field-help-message');
     help.text('');
     help.hide();
-    console.log('hiding');
     if (!val) {
       customSettingsFields.hide();
     } else if (val === '-1') {
@@ -41,7 +39,6 @@
       customSettingsFields.hide();
       data = opt.data();
       coverage = data.coverage;
-      console.log('showing');
       help.text(coverage);
       help.show();
       for (j = 0, len1 = FIELDS.length; j < len1; j++) {
