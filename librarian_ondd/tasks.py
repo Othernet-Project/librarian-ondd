72abc2c8d52d1b294127bb0a6520d60acae3b65a
Branko Vukelic <branko@outernet.is>
2016-02-25 21:14:24 +0100

    Fixes foreign value, and caps used as cache_max
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian_ondd/tasks.py
+++ librarian_ondd/tasks.py
@@ -5,33 +5,38 @@ def query_cache_storage_status(supervisor):
     cache_min = supervisor.config['ondd.cache_min']
     cache_max = supervisor.config['ondd.cache_quota']
     cache_status = supervisor.exts.ondd.get_cache_storage()
-    cache_free = cache_status['free']
-    cache_used = cache_status['used']
+    real_free = cache_status['free']
+    real_used = cache_status['used']
+
+    # First of all, since used space is real, but capacity is virtual
+    # (cache_max), we need to make sure used space does not exceed the
+    # capacity, or we'll get weird results.
+    cache_used = min(real_used, cache_max)
 
     # We want cache to be a certain amount (cache_max). Since this is a virtual
     # capacity, we also have a virtual free space which is the amount of space
     # we want the cache to have (ideally) and the amount of data used in
     # reality.
-    cache_virt_free = cache_max - cache_used
+    virt_free = cache_max - cache_used
 
     # ONDD download cache is shared with other data that is stored on the
     # internal disk. This includes databases, logs, and downloaded files that
     # are not part of the cache (permanent files). The foreign data may
     # actually bite into the cache space. In this case, we need to add to the
     # used space, and subtract from the virtual free space.
-    cache_foreign = min(0, cache_free - cache_virt_free)
+    cache_foreign = abs(min(0, real_free - virt_free))
     cache_used += cache_foreign
-    cahche_virt_free -= cache_foreign
+    virt_free -= cache_foreign
 
     # Sanity check
     assert cache_virt_free + cache_used == cache_max
 
     cache_status = dict(total=cache_max,
-                        free=cache_virt_free,
+                        free=virt_free,
                         used=cache_used)
     supervisor.exts.cache.set('ondd.cache', cache_status)
 
-    if cache_free > cache_min:
+    if real_free > cache_min:
         # We have enough free space, so bail
         return
 
@@ -41,7 +46,7 @@ def query_cache_storage_status(supervisor):
     supervisor.exts.notifications.send(
         # Translators, notification displayed when internal cache storage
         # is running out of disk space
-        _('Cache storage space is getting low. '
+        _('Download capacity is getting low. '
           'Please ask the administrator to take action.'),
         category='ondd_cache',
         dismissable=False,
@@ -50,7 +55,7 @@ def query_cache_storage_status(supervisor):
     supervisor.exts.notifications.send(
         # Translators, notification displayed when internal cache storage
         # is running out of disk space
-        _('Cache storage space is getting low. You will stop receiving '
+        _('Download cache capacity is getting low. You will stop receiving '
           'new content if you run out of storage space. Please move some '
           'content from the internal storage to an external one.'),
         category='ondd_cache',
