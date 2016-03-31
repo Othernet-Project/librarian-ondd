4c36a6af92dc70afd34cc15114ce6710f0d7e764
Branko Vukelic <branko@outernet.is>
2016-03-31 15:07:51 +0200

    PEP8
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian_ondd/tools.py
+++ librarian_ondd/tools.py
@@ -7,4 +7,3 @@ from librarian_core.contrib.templates.decorators import template_helper
 def has_tuner():
     TUNER_DEV_PATH = '/dev/dvb/adapter0/frontend0'
     return os.path.exists(TUNER_DEV_PATH)
-
